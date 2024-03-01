b35b76a70f074319b4328a73d6713dd4e4f9724c
RussellZhou-hub <938311288@qq.com>
2024-03-02 04:50:26 +0800

    create project

---
+++ city_walk/city_walkApp.swift
@@ -0,0 +1,17 @@
+//
+//  city_walkApp.swift
+//  city_walk
+//
+//  Created by VincentZhou on 2024/3/2.
+//
+
+import SwiftUI
+
+@main
+struct city_walkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ city_walkUITests/city_walkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  city_walkUITestsLaunchTests.swift
+//  city_walkUITests
+//
+//  Created by VincentZhou on 2024/3/2.
+//
+
+import XCTest
+
+final class city_walkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
